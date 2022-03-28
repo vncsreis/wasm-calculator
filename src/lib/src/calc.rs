509c2d36973a90f9910bce2641b2cad011583d27
Vinícius <euphonion@tutanota.com>
2022-03-27 23:22:48 -0300

    Added error logging dependency + tests

--- src/lib/src/calc.rs
+++ src/lib/src/calc.rs
@@ -15,14 +15,14 @@ pub fn divide(n1: f64, n2: f64) -> f64 {
 }
 
 pub fn operate(operand: &String, n1: &String, n2: &String) -> f64 {
-    let n1: f64 = n1.parse().unwrap();
-    let n2: f64 = n2.parse().unwrap();
+    let n1: f64 = n1.trim().parse().unwrap();
+    let n2: f64 = n2.trim().parse().unwrap();
 
     match operand.as_str() {
         "+" => return add(n1, n2),
         "-" => return subtract(n1, n2),
         "*" => return multiply(n1, n2),
         "/" => return divide(n1, n2),
-        _ => todo!(),
+        _ => return 0.0,
     }
 }
--- src/lib/src/lib.rs
+++ src/lib/src/lib.rs
@@ -1,5 +1,7 @@
+extern crate console_error_panic_hook;
 extern crate wasm_bindgen;
 
+use std::panic;
 use wasm_bindgen::prelude::*;
 
 mod calc;
@@ -7,6 +9,8 @@ mod utils;
 
 #[wasm_bindgen]
 pub fn calculate(input: String) -> f64 {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     let str_arr = utils::split_string(input);
 
     calc::operate(&str_arr[1], &str_arr[0], &str_arr[2])
@@ -51,13 +55,14 @@ mod calc_main_tests {
     fn calc_test() {
         let test_1 = String::from("2.4 + 2.8");
         let test_2 = String::from("4.7 - 2.6");
-        let test_3 = String::from("2.7 x 2.9");
+        let test_3 = String::from("2.7 * 2.9");
         let test_4 = String::from("8.5 / 2.5");
 
         let test_5 = String::from("2 + 2");
         let test_6 = String::from("4 - 2");
-        let test_7 = String::from("2 x 2");
+        let test_7 = String::from("2 * 2");
         let test_8 = String::from("8 / 2");
+        let test_9 = String::from("-84 + 84");
 
         assert_eq!(calculate(test_1), (2.4 + 2.8));
         assert_eq!(calculate(test_2), (4.7 - 2.6));
@@ -68,6 +73,14 @@ mod calc_main_tests {
         assert_eq!(calculate(test_6), (4.0 - 2.0));
         assert_eq!(calculate(test_7), (2.0 * 2.0));
         assert_eq!(calculate(test_8), (8.0 / 2.0));
+        assert_eq!(calculate(test_9), 0.0);
+    }
+
+    #[test]
+    fn negative_two_digit_number_sum_with_itself() {
+        let test = String::from("-10 + 10");
+
+        assert_eq!(calculate(test), 0.0);
     }
 }
 
@@ -81,6 +94,16 @@ mod utils_tests {
         let expect_vec = ["2.2", "+", "2.2"].map(|x| String::from(x));
 
         let split = split_string(String::from("2.2 + 2.2"));
+
+        assert_eq!(split, expect_vec);
+        assert_eq!(split[1], String::from("+"));
+    }
+
+    #[test]
+    fn split_string_with_negative_number() {
+        let expect_vec = ["-84", "+", "84"].map(|x| String::from(x));
+
+        let split = split_string(String::from("-84 + 84"));
 
         assert_eq!(split, expect_vec);
         assert_eq!(split[1], String::from("+"));
