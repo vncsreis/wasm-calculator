0cbfe46f01423e7d6d17888cb98fb8a6251a2ce4
Vinícius <euphonion@tutanota.com>
2022-03-28 22:26:26 -0300

    Remove vite boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 import Container from './components/Container';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Container>
